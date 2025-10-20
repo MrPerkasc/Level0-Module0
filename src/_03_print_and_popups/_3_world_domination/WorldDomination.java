fd2e8e7053f6dfd3dd53c5a384a1423488ee6d23
MrPerkasc <aliasharrison@gmail.com>
2025-10-20 18:26:21 +0000

    wa

--- src/_03_print_and_popups/_3_world_domination/WorldDomination.java
+++ src/_03_print_and_popups/_3_world_domination/WorldDomination.java
@@ -9,7 +9,17 @@ public class WorldDomination {
 		
 		// 1. Ask the user if they know how to write code.
 
-		// 2. If they say "yes", tell them they will rule the world.
+		// Get the player to enter an adjective
+		String input = JOptionPane.showInputDialog("Do you know how to write code?");
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"nice");
+
+
+		} else {
+			JOptionPane.showMessageDialog(null,"Learn");
+
+			
+		}
 
 		// 3. Otherwise, tell them to sign up for classes at The League.
 
--- src/_03_print_and_popups/_4_quiz_game/QuizGame.java
+++ src/_03_print_and_popups/_4_quiz_game/QuizGame.java
@@ -1,14 +1,41 @@
 package _03_print_and_popups._4_quiz_game;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	public static void main(String[] args) {
 		
 		// Create a variable to hold the user's score. Set it equal to zero. 
 		
 		// ASK A QUESTION AND CHECK THE ANSWER
-		
+		String input = JOptionPane.showInputDialog("is water clear?");
+		int score = 0;
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"correct");
+			score += 1;
+
+		} else {
+			JOptionPane.showMessageDialog(null,"wrong");
+
+			
+		}
+		System.out.println(score);
+		JOptionPane.showMessageDialog(null, score);
+
 				// 2.  Ask the user a question 
-				
+				input = JOptionPane.showInputDialog("is dirt brown?");
+		if (input.equals("yes")) {
+			JOptionPane.showMessageDialog(null,"correct");
+			score += 1;
+
+		} else {
+			JOptionPane.showMessageDialog(null,"wrong");
+
+			
+		}
+		System.out.println(score);
+		JOptionPane.showMessageDialog(null, score);
+
 				// 3.  Use an if statement to check if their answer is correct
 				
 				// 4.  if the user's answer was correct, add one to their score 
